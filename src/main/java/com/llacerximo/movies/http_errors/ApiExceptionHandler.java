5fe7e7abfde61b22132d5970b91d6614c8acef2d
Ximo Llacer <xllaber7@gmail.com>
2023-10-05 12:09:44 +0200

    Feature - Add exception

--- src/main/java/com/llacerximo/movies/http_errors/ApiExceptionHandler.java
+++ src/main/java/com/llacerximo/movies/http_errors/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.llacerximo.movies.http_errors;
 
 import com.llacerximo.movies.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,15 @@ public class ApiExceptionHandler {
     @ResponseBody
     public ErrorMessage notFoundRequest(Exception exception) {
         return new ErrorMessage(exception.getMessage(), HttpStatus.NOT_FOUND.value());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({
+            HttpMessageNotReadableException.class
+    })
+    @ResponseBody
+    public ErrorMessage httpMessageNotReadable(Exception exception) {
+        return new ErrorMessage(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
